3144602daff2444c926ea1c5e09445dda873e2a9
Radha Manickam <radhaarul@gmail.com>
2022-02-25 11:39:16 +0100

    set attributes for titles in course and module

--- Lms.Core/Dto/CoursePutDto.cs
+++ Lms.Core/Dto/CoursePutDto.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -9,6 +10,8 @@ namespace Lms.Core.Dto
     public class CoursePutDto
     {
         public int Id { get; set; }
+        [Required]
+        [MaxLength(20, ErrorMessage = "Length of the Title can't be more than 20")]
         public string Title { get; set; }
         public DateTime StartDate { get; set; }
     }
